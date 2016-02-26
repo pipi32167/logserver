b6908919b4c6abf37f8f7f6b8dec0296600fc572
杨其斌 <pipi32167@gmail.com>
2016-02-26 14:25:00 +0800

    init

---
+++ server.js
@@ -0,0 +1,35 @@
+'use strict';
+let P = require('bluebird');
+let _ = require('underscore');
+let CGModel = P.promisifyAll(require('cg_model'));
+let koa = require('koa');
+let router = require('koa-router')();
+let bodyParser = require('koa-body-parser');
+
+let app = global.app = koa();
+
+require('./lib/utils/appUtil')(app);
+
+app.set('base', __dirname);
+app.set('code', app.require('lib/utils/code'));
+app.set('consts', app.require('lib/utils/consts'));
+app.set('logger', app.require('lib/model/logger').getLogger());
+
+let logger = app.get('logger');
+
+app.set('PassportServer', app.require('lib/servers/passportServer'));
+
+app.require('lib/utils/mysqlUtil').init();
+
+app.require('lib/routes')(router);
+
+app.require('lib/middlewares/log')(app);
+
+app
+  .use(bodyParser())
+  .use(router.routes())
+  .use(router.allowedMethods());
+
+let config = app.require('config/servers.json').logReader;
+app.listen(config.port);
+logger.info(`url: http://${config.host}:${config.port}`)
